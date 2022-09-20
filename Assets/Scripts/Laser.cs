99b3ecb7ee0ea5b43d38668b998e162d943ac2f7
M. Fırat Fidan <103283309+Euphratesff@users.noreply.github.com>
2022-09-20 18:28:37 +0300

    Initial commit

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField]
+    private float _laserSpeed = 8f;
+    void Update()
+    {
+        
+        transform.Translate(Vector3.up * _laserSpeed * Time.deltaTime);
+
+        if (transform.position.y >= 8.97f)
+        {
+            Destroy(this.gameObject);
+        }
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# SpaceShooter
+ Created with "Learn C# by Developing 2D & 3D Games Udemy course by Jonathan Weinberger, Authorized Unity Instructor
